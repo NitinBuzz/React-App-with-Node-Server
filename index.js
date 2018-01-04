732431fb07e672c5fbb1751d4c1afbbfbc084baf
NitinBuzz <reachnitinvenkat@gmail.com>
2018-01-05 00:42:50 +0530

    node express

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get('/proxy/my/path', function(req, res) {
 });
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static('client/public'));
   const path = require('path');
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
