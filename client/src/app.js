7c180d9ff2e99183751a3dc9448e8e6d648b2097
NitinBuzz <reachnitinvenkat@gmail.com>
2018-01-05 00:30:02 +0530

    init

---
+++ client/src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+ReactDOM.render(
+  <div>
+    <a href="/api/server">Go to Server</a>
+    <p>Hello World </p>
+  </div>,
+  document.getElementById('app')
+);
---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hi Server');
+});
+app.get('/api/server', function(req, res) {
+  res.send('Hello World! from server');
+});
+app.get('/proxy/my/path', function(req, res) {
+  res.send('Hello Worldzzzz! from server');
+});
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT);
